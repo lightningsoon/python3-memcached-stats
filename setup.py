dcc0649f8c5f4bec885e2aa62a0c4a4edd3b6be9
Momentum <lightingsoon@163.com>
2020-12-15 04:02:42 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     name='python3-memcached-stats',
     version='0.1',
     description='Python3 class to gather stats and slab keys from memcached via the memcached telnet interface',
-    author='HHM',
+    author='Daniel Rust, Haoming Huang',
     url='https://github.com/lightningsoon/python3-memcached-stats',
     package_dir={'': 'src'},
     py_modules=[
